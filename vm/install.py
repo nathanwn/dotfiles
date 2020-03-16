7b9f7ff0c39b6d19c143d2ae9f03c9eabbe1a77e
nathan-wien <nhat.nguyen.cs17@gmail.com>
2020-03-16 13:09:27 +1000

    Add chrome, xfpanel-switch, and chrome

--- vm/install.py
+++ vm/install.py
@@ -8,6 +8,9 @@ import os
 import subprocess
 
 
+CURL_DES = "~/install"
+
+
 def apt_update():
     os.system("sudo apt update")
 
@@ -17,6 +20,28 @@ def apt_install(package):
     os.system("sudo apt install {0} -y".format(package))
 
 
+def apt_fix_broken():
+    os.system("sudo apt install -fy")
+
+
+def curl_install(app, url):
+    print(">>>>>>>> Installing {0}".format(app))
+    os.system("curl -o ~/install/{0}/{0}.deb -L --create-dirs {1}".format(app, url))
+    os.system("sudo dpkg -i {0}/{1}/{1}.deb".format(CURL_DES, app))
+
+
+def install_ibus():
+    os.system("ibus restart")
+
+
+def install_chrome():
+    os.system("sudo apt install libxss1 libappindicator1 libindicator7")
+    app = "chrome"
+    url = "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+    curl_install(app, url)
+    apt_fix_broken()
+
+
 def install_vim():
     print(">>>>>>>> Installing vim packages")
     os.system("curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim")
@@ -24,9 +49,15 @@ def install_vim():
 
 
 def install_vscode():
-    print(">>>>>>>> Installing vscode")
-    os.system("curl -o ~/install/code/code.deb -L --create-dirs http://go.microsoft.com/fwlink/?LinkID=760868")
-    os.system("sudo dpkg -i ~/install/code/code.deb")
+    app = "code"
+    url = "http://go.microsoft.com/fwlink/?LinkID=760868"
+    curl_install(app, url)
+
+
+def install_xfpanelswitch():
+    app = "xfpanel-switch"
+    url = "http://launchpadlibrarian.net/367095882/xfpanel-switch_1.0.7-0ubuntu2_all.deb"
+    curl_install(app, url)
 
 
 apt_packages = [
@@ -50,13 +81,19 @@ apt_packages = [
     "zathura"                   ,
     # svn
     "subversion"                ,
+    # unikey
+    "ibus-unikey"               ,
+    # guest-addition for virtualbox
+    "dkms"                      ,
+    # xfce4
+    "papirus-icon-theme"        ,
     # other
-    "software-properties-common"
+    "software-properties-common",
 ]
 
 
 apt_update()
 
 
-for package in packages:
+for package in apt_packages:
     apt_install(package)
