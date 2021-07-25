3292b1cecb083d92d02b08b7cab6faaaff68a715
nathan-wien <nhat.nguyen.cs17@gmail.com>
2021-07-25 16:25:53 +1000

    [nvim] Enable installing plug automatically

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -15,6 +15,16 @@ vim.g.maplocalleader = t'<Space>'
 -------------------------------------------------------------------------------
 -- Plugins
 -------------------------------------------------------------------------------
+vim.api.nvim_exec(
+	[[
+		if empty(glob('~/.config/nvim/autoload/plug.vim'))
+			silent !curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+			autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
+		endif
+	]],
+	false
+)
+
 vim.fn['plug#begin']()
   -- UTILITIES
   -- Git
