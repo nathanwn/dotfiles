2ba7fc143b4ff604aec6bbebadc6d3c3344f3fd5
nathan-wien <nhat.nguyen.cs17@gmail.com>
2021-08-05 21:57:42 +1000

    [ubuntu20] Add some useful programs

--- ubuntu20/zsh/.zsh/local.zsh
+++ ubuntu20/zsh/.zsh/local.zsh
@@ -11,9 +11,13 @@ export PATH="/usr/lib/dart/bin:$PATH"
 # go
 export PATH="/usr/local/go/bin:$PATH"
 export PATH="$(go env GOPATH)/bin:$PATH"
+# infer
+export PATH="$HOME/bin/infer/infer-linux64-v1.1.0/bin:$PATH"
 # pascal
 export PATH="$HOME/bin/fpc-3.2.0/bin:$PATH"
 # spotifyd
 export PATH="$HOME/bin/spotifyd/target/release:$PATH"
+# qpdf
+export PATH="$HOME/bin/qpdf/qpdf/build:$PATH"
 
 alias gpath="pwd | xclip -selection clipboard"
