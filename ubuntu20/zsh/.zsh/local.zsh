1e3f797bcaf53574f527b2d6fa1da05ee0af9a64
nathan-wien <nhat.nguyen.cs17@gmail.com>
2021-11-14 20:32:21 +1000

    [zsh] Update path to nvim

--- ubuntu20/zsh/.zsh/local.zsh
+++ ubuntu20/zsh/.zsh/local.zsh
@@ -7,6 +7,8 @@ export PATH="$HOME/.local/bin:$PATH"
 # Shell programs
 # exa
 export PATH="$HOME/bin/exa/exa-linux-x86_64-v0.10.0/bin:$PATH"
+# nvim
+export PATH="$HOME/bin/nvim-linux64/bin:$PATH"
 
 # Languages & Tools
 # c++
@@ -38,6 +40,7 @@ export PATH="$HOME/bin/spotifyd/target/release:$PATH"
 export PATH="$HOME/bin/qpdf/qpdf/build:$PATH"
 
 alias gpath="pwd | xclip -selection clipboard"
+alias rellis="cd $HOME/work/uq/engg4811/RELLIS-3D"
 
 function zath() { nohup zathura $1 > /dev/null & disown & }
 function imgview() { nohup sxiv $1 > /dev/null & disown & }
