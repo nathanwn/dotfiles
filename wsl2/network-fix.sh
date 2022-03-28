70f9e09def493ffc0ae2415690dfbb1392bd78eb
nathan-wien <nhat.nguyen.cs17@gmail.com>
2022-03-28 20:31:59 +1000

    [wsl2] Fix network issue when using npm

---
+++ wsl2/network-fix.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo rm /etc/resolv.conf
+sudo bash -c 'echo "nameserver 8.8.8.8" > /etc/resolv.conf'
+sudo bash -c 'echo "[network]" > /etc/wsl.conf'
+sudo bash -c 'echo "generateResolvConf = false" >> /etc/wsl.conf'
+sudo chattr +i /etc/resolv.conf
