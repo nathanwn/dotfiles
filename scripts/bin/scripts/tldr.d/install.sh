8c6b73155293ee33824030dde57737cba8cf3fed
nathan-wien <nhat.nguyen.cs17@gmail.com>
2022-03-28 20:31:47 +1000

    [scripts] add some useful scripts

--- scripts/bin/scripts/tldr.d/install.sh
+++ scripts/bin/scripts/tldr.d/install.sh
@@ -1,6 +1,5 @@
+#!/usr/bin/env bash
+
 git clone https://github.com/tldr-pages/tldr.git
-
-find tldr/pages | awk -F '/' '{print $4}' | awk -F '.' '{print $1}' | awk NF | sort | tee entries.txt
-
+ldfn tldr/pages | tee entries.txt
 rm -rf tldr
-
