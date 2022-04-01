2aab645c2a8d020b81ea2de3f784fd03150f0489
nathan-wien <nhat.nguyen.cs17@gmail.com>
2022-04-01 11:14:46 +1000

    [nvim] Fix null-ls issue

--- nvim/.config/nvim/lua/nathan-wien/lsp/servers/null-ls.lua
+++ nvim/.config/nvim/lua/nathan-wien/lsp/servers/null-ls.lua
@@ -1,5 +1,17 @@
 local builtins = require("null-ls").builtins
 
+local should_attach = function(bufnr)
+  local disabled_filetypes = { "cpp" }
+
+  for _, ft in ipairs(disabled_filetypes) do
+    if vim.api.nvim_buf_get_option(bufnr, "filetype") == ft then
+      return false
+    end
+  end
+
+  return true
+end
+
 return {
   sources = {
     -- Lua
@@ -15,4 +27,5 @@ return {
     builtins.diagnostics.mypy,
   },
   diagnostics_format = "[#{c}] #{m} (#{s})",
+  should_attach = should_attach,
 }
