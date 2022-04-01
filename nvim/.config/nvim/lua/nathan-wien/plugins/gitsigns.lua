84b22303317a02d192d3c7fa2970ffea121ccc4f
nathan-wien <nhat.nguyen.cs17@gmail.com>
2022-04-01 11:15:21 +1000

    [nvim] Make gitsigns annotations a bit more visible

--- nvim/.config/nvim/lua/nathan-wien/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/nathan-wien/plugins/gitsigns.lua
@@ -1,5 +1,37 @@
 return function()
   require("gitsigns").setup({
+    signs = {
+      add = {
+        hl = "GitSignsAdd",
+        text = "▌", -- │
+        numhl = "GitSignsAddNr",
+        linehl = "GitSignsAddLn",
+      },
+      change = {
+        hl = "GitSignsChange",
+        text = "▌", -- │
+        numhl = "GitSignsChangeNr",
+        linehl = "GitSignsChangeLn",
+      },
+      delete = {
+        hl = "GitSignsDelete",
+        text = "_",
+        numhl = "GitSignsDeleteNr",
+        linehl = "GitSignsDeleteLn",
+      },
+      topdelete = {
+        hl = "GitSignsDelete",
+        text = "‾",
+        numhl = "GitSignsDeleteNr",
+        linehl = "GitSignsDeleteLn",
+      },
+      changedelete = {
+        hl = "GitSignsChange",
+        text = "~",
+        numhl = "GitSignsChangeNr",
+        linehl = "GitSignsChangeLn",
+      },
+    },
     on_attach = function(bufnr)
       local function map(mode, lhs, rhs, opts)
         opts = vim.tbl_extend("force", { noremap = true, silent = true }, opts or {})
