ae3d8c6fdf48e6607c23754f43c2868b657e53d3
nathan-wien <49882031+nathan-wien@users.noreply.github.com>
2023-03-01 08:26:33 +1000

    wezterm: start using

---
+++ config/default/wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require("wezterm")
+
+return {
+	color_scheme = "PaperColorLight (Gogh)",
+	enable_tab_bar = false,
+	font = wezterm.font("JetBrains Mono Nerd Font"),
+	font_size = 11,
+}
