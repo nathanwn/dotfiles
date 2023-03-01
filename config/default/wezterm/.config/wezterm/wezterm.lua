36c7c8a3c08a9e10f09f6fe65cfc5a0a45d31550
nathan-wien <49882031+nathan-wien@users.noreply.github.com>
2023-03-02 07:57:11 +1000

    wezterm: disable ligature

--- config/default/wezterm/.config/wezterm/wezterm.lua
+++ config/default/wezterm/.config/wezterm/wezterm.lua
@@ -1,8 +1,10 @@
 local wezterm = require("wezterm")
 
 return {
-	color_scheme = "PaperColorLight (Gogh)",
-	enable_tab_bar = false,
-	font = wezterm.font("JetBrains Mono Nerd Font"),
-	font_size = 11,
+  color_scheme = "PaperColorLight (Gogh)",
+  enable_tab_bar = false,
+  font = wezterm.font("JetBrains Mono Nerd Font"),
+  font_size = 11,
+  -- Disable ligature
+  harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
 }
