3fc2b5ae1371f7e4f47162d997e7e865eb13e862
nathan-wien <49882031+nathan-wien@users.noreply.github.com>
2023-03-04 14:07:55 +1000

    [wezterm]: fix font name

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 return {
   color_scheme = "PaperColorLight (Gogh)",
   enable_tab_bar = false,
-  font = wezterm.font("JetBrains Mono Nerd Font"),
+  font = wezterm.font("JetBrainsMono Nerd Font"),
   font_size = 11,
   -- Disable ligature
   harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
