0df8c890221ea2e36a053cbf0c9628b8c62437ed
nathan-wien <49882031+nathan-wien@users.noreply.github.com>
2023-03-18 23:03:05 +1000

    feat: add script to free a port

---
+++ home/bin/freeport.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Kill a process that is holding onto a port.
+# Requires netstat.
+
+set -eu
+
+if [[ "$#" -ne 1 ]]; then
+  echo "Usage: freeport.sh <port number>"
+  exit 1
+fi
+
+exec netstat --tcp --udp --listening --program --numeric \
+  | grep "$1" \
+  | awk -F ' ' '{print $7}' \
+  | awk -F '/' '{print $1}'
