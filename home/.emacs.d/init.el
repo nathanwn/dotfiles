4db04315068fa4b74737feaad27d467c05954382
nathan-wien <49882031+nathan-wien@users.noreply.github.com>
2023-11-19 17:03:40 +1000

    update emacs

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -24,8 +24,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("7d26b8ba9cfe187712ba9816307809d22bdc202c2da21ea800c26a6d2151a23b" default))
  '(package-selected-packages
-   '(elisp-autofmt org-contrib org-roam diff-hl flycheck vertico company-box xref company company-mode cmake-mode which-key use-package rainbow-delimiters org-bullets lsp-ui lsp-treemacs lsp-pyright ivy-rich helpful general evil-collection counsel-projectile command-log-mode)))
+   '(toc-org evil-org org-superstar org-superstar: org-tempo elisp-autofmt org-contrib org-roam diff-hl flycheck vertico company-box xref company company-mode cmake-mode which-key use-package rainbow-delimiters org-bullets lsp-ui lsp-treemacs lsp-pyright ivy-rich helpful general evil-collection counsel-projectile command-log-mode))
+ '(safe-local-variable-values
+   '((eval setq-local org-roam-db-location
+	   (expand-file-name "org-roam.db" org-roam-directory))
+     (eval setq-local org-roam-directory
+	   (expand-file-name
+	    (locate-dominating-file default-directory ".dir-locals.el"))))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -41,9 +49,12 @@
 (tool-bar-mode -1)          ; Disable the toolbar
 (tooltip-mode -1)           ; Disable tooltips
 (menu-bar-mode -1)          ; Disable the menu bar
+(blink-cursor-mode -1)      ; Disable cursor blinking
 ; (set-fringe-mode 10)        ; Give some breathing room
 ;; Font
 (set-face-attribute 'default nil :font "ComicShannsMono Nerd Font" :height 132)
+(set-face-attribute 'fixed-pitch nil :font "ComicShannsMono Nerd Font" :height 132)
+(set-face-attribute 'variable-pitch nil :font "Source Sans Pro" :height 140)
 ;; Theme
 (load-theme 'tango t)
 ;; Make ESC quit prompts
@@ -78,7 +89,7 @@
 
 ;; Ivy
 (use-package ivy
-  ;; :diminish  ;; hide the minor mode from mode line
+  :diminish  ;; hide the minor mode from mode line
   :bind (("C-s" . swiper)
          :map ivy-minibuffer-map
          ("TAB" . ivy-alt-done)
@@ -96,10 +107,12 @@
   (ivy-mode 1))
 ;; Provide useful descriptions in ivy minibuffers
 (use-package ivy-rich
+  :diminish
   :init
   (ivy-rich-mode 1))
 
 (use-package counsel
+  :diminish
   :bind (
      ("M-x" . counsel-M-x)
      ("C-x b" . counsel-ibuffer)
@@ -135,13 +148,14 @@
 )
 
 (use-package counsel-projectile
+  :diminish
   :config (counsel-projectile-mode))
 
 ;; Key-bindings
 ;; Which-key
 (use-package which-key
   :defer 0
-  :diminish which-key-mode
+  :diminish ;; which-key-mode
   :config
   (which-key-mode)
   (setq which-key-idle-delay 1))
@@ -149,6 +163,7 @@
 ;; Some useful resources:
 ;; https://github.com/noctuid/evil-guide#bare-minimum-emacs-knowledge
 (use-package evil
+  :diminish
   :after (projectile)
   :init
   ;; use ctrl-h + v to view what each option does
@@ -166,6 +181,10 @@
 
   (evil-set-initial-state 'messages-buffer-mode 'normal)
   (evil-set-initial-state 'dashboard-mode 'normal)
+  ;; enable redo with C-r
+  (evil-set-undo-system 'undo-redo)
+  ;; make :q/:wq kill the buffer, and not Emacs
+  (global-set-key [remap evil-quit] 'kill-buffer-and-window)
   ;; set leader key in normal state
   ; (evil-set-leader 'normal (kbd "SPC"))
   ;; set local leader
@@ -199,17 +218,20 @@
   (my-leader-def
     :states '(normal visual)
     :keymaps 'override
-    "ff" 'projectile-find-file
-    "fg" 'counsel-projectile-rg
-    "fp" 'projectile-switch-project
-    "fb" 'counsel-ibuffer
-    "fo" 'counsel-minibuffer-history
-    "f?" 'counsel-M-x
+    "f"  '(:ignore t :wk "Find")
+    "ff" '(projectile-find-file :wk "Find file (project)")
+    "fg" '(counsel-projectile-rg :wk "Ripgrep (project)")
+    "fp" '(projectile-switch-project :wk "Switch project")
+    "fb" '(counsel-ibuffer :wk "Find buffer")
+    "fc" '(counsel-grep :wk "Grep")
+    "fo" '(counsel-minibuffer-history :wk "Find prev")
+    "fx" '(counsel-M-x :wk "Find command")
   )
   ;; lsp
   (my-leader-def
     :states '(normal visual)
-    :keymaps 'override
+    ;; :keymaps 'override
+    :keymaps 'eglot-mode-map
     "gd" 'eglot-find-declaration
     "gt" 'eglot-find-typeDefinition
     "gi" 'eglot-find-implementation
@@ -220,9 +242,15 @@
   (my-leader-def
     :states '(normal visual)
     :keymaps 'org-mode-map
-    "rf" 'org-roam-node-find
-    "ri" 'org-roam-node-insert
-    "rt" 'org-roam-buffer-toggle
+    "gd" 'org-open-at-point
+  )
+  (my-leader-def
+    :states '(normal visual)
+    :keymaps 'override
+    "r"  '(:ignore t :wk "Org-roam")
+    "rf" '(org-roam-node-find :wk "Find")
+    "ri" '(org-roam-node-insert :wk "Insert")
+    "rt" '(org-roam-buffer-toggle :wk "Buffer toggle")
   )
   ;; buffer navigation
   (general-nmap
@@ -233,26 +261,88 @@
     "]c" 'diff-hl-next-hunk
     "[c" 'diff-hl-previous-hunk
   )
+  ;; zooming
+  (global-set-key (kbd "C-=") 'text-scale-increase)
+  (global-set-key (kbd "C--") 'text-scale-decrease)
 )
 
 
 ;; Org-mode
 ;; This is the nongnu version
+(defun my-org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (auto-fill-mode 0)
+  (visual-line-mode 1)
+  ;; stop emacs from automatically 
+  (electric-indent-mode -1)
+  (setq evil-auto-indent nil)
+  ;; modify default ellipsis marker (...)
+  (setq org-ellipsis " ▾")
+  ;; hide emphasis markers
+  (setq org-hide-emphasis-markers t)
+  ;; syntax highlighting in org src blocks
+  (setq org-src-fontify-natively t)
+  ;; highlight inline LaTeX
+  (setq org-highlight-latex-and-related '(native))
+  ;; automatically change bullet type when indenting
+  ;; Ex: indenting a + makes the bullet a *.
+  (setq org-list-demote-modify-bullet
+        '(("+" . "-") ("-" . "+") ("+" . "-")))
+  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-table nil  :inherit 'fixed-pitch)
+  (set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-indent nil :inherit '(org-hide fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
+)
+
 (use-package org-contrib
+  :hook (org-mode . my-org-mode-setup)
   :config
-  (setq org-ellipsis " ▾"           ;; modify default ellipsis marker (...)
-        org-hide-emphasis-markers t ;; hide emphasis markers
-  )
+  (require 'ox-md nil t)
 )
+;; useful src snippets
+(require 'org-tempo)
+(add-to-list 'org-structure-template-alist '("sh" . "src shell"))
+(add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+(add-to-list 'org-structure-template-alist '("py" . "src python"))
+;; fonts
+(dolist (face '((org-level-1 . 2)
+                (org-level-2 . 1.6)
+                (org-level-3 . 1.3)
+                (org-level-4 . 1.2)
+                (org-level-5 . 1.1)
+                (org-level-6 . 1.1)
+                (org-level-7 . 1.05)
+                (org-level-8 . 1.05)))
+    ;; (set-face-attribute (car face) nil :font "Source Sans Pro" :weight 'regular :height (cdr face))
+)
+;; Make sure org-indent face is available
+(require 'org-indent)
 ;; bullet
-(use-package org-bullets
+;; (use-package org-bullets
+;;   :after org
+;;   :hook (org-mode . org-bullets-mode)
+;;   :init
+;;   (add-hook 'org-mode-hook
+;;       (lambda ()
+;;         (toggle-truncate-lines nil) ))
+;; )
+(use-package org-superstar
   :after org
-  :hook (org-mode . org-bullets-mode)
-  :init
-  (add-hook 'org-mode-hook
-      (lambda ()
-        (toggle-truncate-lines nil) ))
+  :hook (org-mode . org-superstar-mode)
+  :custom
+  ;; (org-superstar-remove-leading-stars t)
+  (org-superstar-headline-bullets-list '("◉" "○" "●" "○" "●" "○" "●"))
 )
+(use-package toc-org
+  :commands toc-org-enable
+  :init (add-hook 'org-mode-hook 'toc-org-enable))
 ;; org-roam
 (use-package org-roam
   :ensure t
@@ -279,7 +369,7 @@
 ;; (use-package lsp-treemacs
 ;;   :after lsp)
 (use-package eglot
-    :hook (prog-mode . eglot-ensure)
+  :hook (prog-mode . eglot-ensure)
 )
 ;; (use-package vertico
 ;;     :config (vertico-mode t)
@@ -301,18 +391,25 @@
 
 ;; autocompletion
 (use-package company
+  :diminish
   :hook (prog-mode . company-mode)
-  :bind (:map company-active-map ("<tab>" . company-complete-selection))
+  :bind (:map company-active-map ("<tab>" . company-complete-common-or-cycle))
         ;; use this if you want tab for cycling
         ;; (:map company-active-map ("<tab>" . company-complete-common-or-cycle))
+        ;; use this if you want tab for completion
+        ;; (:map company-active-map ("<tab>" . company-complete-selection))
   :custom
   (company-minimum-prefix-length 1)
   (company-idle-delay 0.0))
 
 (use-package company-box
-    :hook (company-mode . company-box-mode))
+  :diminish
+  :hook (company-mode . company-box-mode))
 
 ;; git
 (use-package diff-hl
   :init (global-diff-hl-mode)
 )
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'papercolor t)
