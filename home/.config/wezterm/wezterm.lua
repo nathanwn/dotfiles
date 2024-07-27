a25ef8a45df1b28f3cd634ae2674065281b3f3da
nathan-wien <49882031+nathan-wien@users.noreply.github.com>
2024-07-27 17:07:06 +1000

    clean up wezterm config

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -1,41 +1,17 @@
 local wezterm = require("wezterm")
 
--- My version of tokyonight-day
--- https://wezfurlong.org/wezterm/config/appearance.html#defining-your-own-colors
--- local my_tokyonight_day = wezterm.get_builtin_color_schemes()["tokyonight_day"]
-
--- my_tokyonight_day.foreground = "#444444"
--- my_tokyonight_day.background = "#f7f7f7"
-
--- -- red
--- my_tokyonight_day.ansi[2] = "#C02020"
--- my_tokyonight_day.brights[2] = "#FF0000"
-
--- -- green
--- my_tokyonight_day.ansi[3] = "#008000"
--- my_tokyonight_day.brights[3] = "#20C020"
-
--- https://wezfurlong.org/wezterm/colorschemes/index.html
-local color_scheme = "PaperColorLight (Gogh)"
-
 return {
   check_for_updates = false,
-  color_scheme = color_scheme,
-  -- color_scheme = "Tokyo Night",
-  -- color_schemes = {
-  --   my_tokyonight_day = my_tokyonight_day,
-  -- },
-  -- color_scheme = "my_tokyonight_day",
+  color_scheme = "tokyonight_day",
   enable_tab_bar = false,
   -- To get all system fonts: wezterm ls-fonts --list-system
   font = wezterm.font_with_fallback({
-    "DejaVuSansMono Nerd Font",
-    "ComicShannsMono Nerd Font",
     "JetBrainsMono Nerd Font",
+    "ComicShannsMono Nerd Font",
+    "DejaVuSansMono Nerd Font",
     "RobotoMono Nerd Font",
     "Iosevka Nerd Font",
     "FiraCode Nerd Font",
-    "JetBrainsMono Nerd Font Mono",
   }),
   font_size = 11,
   -- Disable ligature
