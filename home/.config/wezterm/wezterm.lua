58b443a71df3b1145572a392754190eba9c7ea7d
nathanwn <49882031+nathanwn@users.noreply.github.com>
2026-07-04 22:35:32 +1000

    wez

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -1,46 +1,37 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
 
+-- To get all system fonts: wezterm ls-fonts --list-system
 local font_family
+-- font_family = "BlexMono Nerd Font"
+-- font_family = "Comic Mono"
 -- font_family = "Consolas 7NF"
 -- font_family = "Consolas"
 -- font_family = "Cousine Nerd Font"
--- font_family = "JetBrainsMono Nerd Font"
--- font_family = "MesloLGM Nerd Font"
+-- font_family = "Google Sans Code"
+-- font_family = "IBM Plex Mono"
 -- font_family = "Inconsolata Nerd Font"
+-- font_family = "JetBrainsMono Nerd Font"
+-- font_family = "Menlo"
+-- font_family = "MesloLGM Nerd Font"
+-- font_family = "Monaco"
+-- font_family = "Mononoki Nerd Font"
+-- font_family = "Mononoki"
 -- font_family = "NotoSansM Nerd Font"
-font_family = "RecMonoLinear Nerd Font"
-font_family = "RobotoMono Nerd Font"
-
-local font_size = 16
-
-if wezterm.hostname() == "ubox" then
-  font_size = 11.5
-  -- font_size = 12.5
-end
+-- font_family = "RecMonoLinear Nerd Font"
+-- font_family = "RobotoMono Nerd Font"
+font_family = "JetBrains Mono"
+local font_with_fallback = {
+  font_family,
+}
+local font_size = 17
+local line_height = 1.1
 
 -- https://wezfurlong.org/wezterm/colorschemes/index.html
 local color_scheme
--- local color_scheme = "Equilibrium Gray Light (base16)"
--- local color_scheme = "Monokai (base16)"
--- local color_scheme = "OneDark (base16)"
--- local color_scheme = "One Dark (Gogh)"
--- local color_scheme = "One Light (base16)"
--- local color_scheme = "Papercolor Light (Gogh)"
--- local color_scheme = "Solarized (dark) (terminal.sexy)"
--- local color_scheme = "Solarized Dark (Gogh)"
-color_scheme = "Tokyo Night Storm"
-
-local font_with_fallback = {
-  font_family,
-  "JetBrainsMono Nerd Font",
-  "ComicShannsMono Nerd Font",
-  "DejaVuSansMono Nerd Font",
-  "FiraCode Nerd Font",
-  "Iosevka Nerd Font",
-  "JetBrainsMono Nerd Font Mono",
-  "RobotoMono Nerd Font",
-}
+color_scheme = "catppuccin-latte"
+-- color_scheme = "NvimLight"
+-- color_scheme = "Tokyo Night Storm"
 
 local config = {
   check_for_updates = false,
@@ -48,22 +39,22 @@ local config = {
   -- color_scheme = "PaperColor Light (base16)",
   -- color_scheme = "Papercolor Light (Gogh)",
   enable_tab_bar = false,
-  -- To get all system fonts: wezterm ls-fonts --list-system
   font = wezterm.font_with_fallback(font_with_fallback),
   font_size = font_size,
+  line_height = line_height,
   -- Disable ligature
   harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
   -- Disable bell
   audible_bell = "Disabled",
   -- Cursor coloring
   force_reverse_video_cursor = true,
-  font_rules = {
-    {
-      font = wezterm.font_with_fallback(font_with_fallback),
-      intensity = "Bold",
-      weight = "Normal",
-    },
-  },
+  -- font_rules = {
+  --   {
+  --     font = wezterm.font_with_fallback(font_with_fallback),
+  --     intensity = "Bold",
+  --     weight = "Regular",
+  --   },
+  -- },
 }
 
 config.keys = {
@@ -103,6 +94,15 @@ if wezterm.hostname() == "ubox" then
   config.freetype_load_flags = "NO_HINTING"
   -- https://github.com/wezterm/wezterm/issues/3565
   config.front_end = "OpenGL"
+end
+
+if wezterm.target_triple == "aarch64-apple-darwin" then
+  -- Tweak font on mac
+  -- config.front_end = 'OpenGL'
+  -- config.freetype_load_target = 'Light'
+  -- config.freetype_render_target = 'HorizontalLcd'
+  config.freetype_load_flags = "NO_HINTING"
+  -- config.cell_width = 0.9
 end
 
 return config
