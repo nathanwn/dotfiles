b1cb3914b5c2ae37b6d5ca7932b1a4aa4b007103
nathanwn <49882031+nathanwn@users.noreply.github.com>
2026-09-02 22:58:32 +1000

    nvim: enable format-on-save for go

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -602,6 +602,11 @@ require("lazy").setup({
     config = function()
       require("conform").setup({
         notify_on_error = false,
+        format_on_save = function(bufnr)
+          if vim.bo[bufnr].filetype == "go" then
+            return {}
+          end
+        end,
         formatters_by_ft = {
           cpp = { "clang-format" },
           go = { "gofmt" },
